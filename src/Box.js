b56ba571daafd9adaab46ab5b48c7a3478d458cd
michaelcbarilla <michaelcbarilla@gmail.com>
2021-02-25 15:41:48 +0000

    change styling of board and boxes

--- src/Box.js
+++ src/Box.js
@@ -6,6 +6,8 @@ export function Box(props) {
 
   
   return (
-      <div className="box" onClick={props.onClick}>{props.piece}</div>
+      <div className="box" onClick={props.onClick}>
+        {props.piece === 'X' ? <span className="piece x">{props.piece}</span> : <span className="piece o">{props.piece}</span>}
+      </div>
   );
 }
