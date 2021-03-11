4dd8753b17ddca6d3b84f9ae047c00b5c6d2c011
michaelcbarilla <michaelcbarilla@gmail.com>
2021-03-11 19:31:01 +0000

    finish eslinting Users.js

--- src/Users.js
+++ src/Users.js
@@ -1,36 +1,44 @@
-import "./Users.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Users.css';
 
-export function Users(props) {
-  if (props.players.length === 1) {
+function Users(props) {
+  const {
+    players,
+  } = props;
+  if (players.length === 1) {
     return (
       <div className="user-container">
         <h2>Player List</h2>
-        <p>Player X: {props.players[0][0]}</p>
+        <p>{`Player X: ${players[0][0]}`}</p>
       </div>
     );
-  } else if (props.players.length === 2) {
-    return (
-      <div className="user-container">
-        <h2>Player List</h2>
-        <p>Player X: {props.players[0][0]}</p>
-        <p>Player O: {props.players[1][0]}</p>
-      </div>
-    );
-  } else if (props.players.length >= 2) {
-    return (
-      <div className="user-container">
-        <h2>Player List</h2>
-        <p>Player X: {props.players[0][0]}</p>
-        <p>Player O: {props.players[1][0]}</p>
-        <h5>Queue</h5>
-        {props.players.map((player, index) => {
-          if (index > 1) {
-            return <p key={index}>{--index + " " + player[0]}</p>;
-          }
-        })}
-      </div>
-    );
-  } else {
-    return null;
   }
+  if (players.length === 2) {
+    return (
+      <div className="user-container">
+        <h2>Player List</h2>
+        <p>{`Player X: ${players[0][0]}`}</p>
+        <p>{`Player O: ${players[1][0]}`}</p>
+      </div>
+    );
+  }
+  if (players.length >= 2) {
+    return (
+      <div className="user-container">
+        <h2>Player List</h2>
+        <p>{`Player X: ${players[0][0]}`}</p>
+        <p>{`Player O: ${players[1][0]}`}</p>
+        <h5>Queue</h5>
+        {players.slice(2).map((player, index) => <p key={index}>{`${index - 1} ${player[0]}`}</p>)}
+      </div>
+    );
+  }
+  return null;
 }
+
+Users.propTypes = {
+  players: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)).isRequired,
+};
+
+export default Users;
