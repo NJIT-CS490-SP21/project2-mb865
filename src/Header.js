ed5bb21fde594ee5cc86e478f74a2c3c96d3bf7b
michaelcbarilla <michaelcbarilla@gmail.com>
2021-03-11 16:33:18 +0000

    finish eslinting header.js

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
-import React from "react";
-import "./Header.css";
+import React from 'react';
+import './Header.css';
 
-export function Header() {
+function Header() {
   return (
     <header className="container">
       <h1>Tic-tac-toe Online</h1>
     </header>
   );
 }
+
+export default Header;
